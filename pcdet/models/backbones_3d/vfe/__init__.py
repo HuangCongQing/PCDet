ab4cacad6f05c0365897dbdbf923705f5854a7bf
HuangCongQing <1756260160@qq.com>
2021-06-29 04:27:32 +0000

    add

--- pcdet/models/backbones_3d/vfe/__init__.py
+++ pcdet/models/backbones_3d/vfe/__init__.py
@@ -1,7 +1,7 @@
 from .vfe_template import VFETemplate
 from .mean_vfe import MeanVFE
 from .pillar_vfe import PillarVFE
-from .mean_vde_trpd import MeanVFETrPD
+from .mean_vfe_trpd import MeanVFETrPD
 
 __all__ = {
     'VFETemplate': VFETemplate,
